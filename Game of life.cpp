f4cee484a0f916a47ed7dcb04cc3c811144aa329
GabryTremo <gabriele.tremolanti@gmail.com>
2025-03-25 19:28:06 +0100

    a volte funziona

--- Game of life.cpp
+++ Game of life.cpp
@@ -26,17 +26,127 @@ void stampa_matrice(int **mat){
 	
 }
 
+bool qualcuno_vivo(int **mat){
+	
+	for (int i=0; i<100; i++){
+		for (int j=0; j<100; j++){
+			if(mat[i][j]==1){
+				return true;
+			}
+		}
+	}
+	return false;
+	
+}
+
+void controlla_vicini(int **mat, int **adj_mat, int size){
+	
+	/*
+	for (int i=1; i<size-2; i++){
+		for (int j=1; j<size-1; j++){
+			adj_mat[i][j] = mat[i-1][j-1] + mat[i-1][j] + mat[i-1][j+1] +
+							mat[i][j-1] + mat[i][j+1] +
+							mat[i+1][j-1] + mat[i+1][j] + mat[i+1][j+1];
+		}
+	}
+	
+	for (int i=1; i<size-2; i++){
+		
+			adj_mat[i][0] = mat[i-1][size-1] + mat[i-1][0] + mat[i-1][1] +
+							mat[i][size-1] + mat[i][1] +
+							mat[i+1][size-1] + mat[i+1][0] + mat[i+1][1];
+		
+	}
+	
+	for (int i=1; i<size-2; i++){
+		
+			adj_mat[i][size-1] = mat[i-1][size-2] + mat[i-1][size-1] + mat[i-1][0] +
+							mat[i][size-2] + mat[i][0] +
+							mat[i+1][size-2] + mat[i+1][size-1] + mat[i+1][0];
+		
+	}
+	
+	for (int j=1; j<size-2; j++){
+		
+			adj_mat[0][j] = mat[size-1][j-1] + mat[size-1][j] + mat[size-1][j+1] +
+							mat[0][j-1] + mat[0][j+1] +
+							mat[1][j-1] + mat[1][j] + mat[1][j+1];
+		
+	}
+	
+	for (int j=1; j<size-2; j++){
+		
+			adj_mat[size-1][j] = mat[size-2][j-1] + mat[size-2][j] + mat[size-2][j+1] +
+							mat[size-1][j-1] + mat[size-1][j+1] +
+							mat[0][j-1] + mat[0][j] + mat[0][j+1];
+		
+	}
+	*/
+	
+	for (int i=0; i<size-1; i++){
+		for (int j=0; j<size-1; j++){
+			adj_mat[i][j] = mat[(100+i-1)%100][(100+j-1)%100] + mat[(100+i-1)%100][j] + mat[(100+i-1)%100][(j+1)%100] +
+							mat[i][(100+j-1)%100] + mat[i][(j+1)%100] +
+							mat[(i+1)%100][(100+j-1)%100] + mat[(i+1)%100][j] + mat[(i+1)%100][(j+1)%100];
+		}
+	}
+	
+	
+}
+
+void aggiorna_matrice(int **mat, int **new_mat, int size){
+	
+	int **adjacency_mat = new int*[size];
+	for (int i=0; i<size; i++){
+		adjacency_mat[i] = new int[size];
+	}
+	
+	controlla_vicini(mat, adjacency_mat, size);
+	
+	for (int i=0; i<100; i++){
+		for (int j=0; j<100; j++){
+			if(mat[i][j]==1){
+				if (adjacency_mat[i][j]<2 || adjacency_mat[i][j]>3){
+					new_mat[i][j] = 0;
+				}
+				else{
+					new_mat[i][j] = 1;
+				}
+			}
+			else if(mat[i][j]==0 && adjacency_mat[i][j]==3){
+				new_mat[i][j] = 1;
+			}
+			else{
+				new_mat[i][j] = 0;
+			}
+		}
+	}
+	
+	for (int i = 0; i < size; i++) {
+        delete[] adjacency_mat[i];
+    }
+    delete[] adjacency_mat;
+}
+
 
 int main() {
 	
 	int *pInitCond = NULL;
 	int init_cond_size;
-	int **mat = NULL;
+	int **mat = NULL, **new_mat = NULL;
 	int size = 100;
+	int max_iter = 10;
+	int cont = 0;
+	
 	
 	mat = new int*[size];
 	for (int i=0; i<size; i++){
 		mat[i] = new int[size];
+	}
+	
+	new_mat = new int*[size];
+	for (int i=0; i<size; i++){
+		new_mat[i] = new int[size];
 	}
 	
 	
@@ -49,19 +159,31 @@ int main() {
 		std::cin>>pInitCond[i+1];
 	}
 	
-	crea_matrice(mat, pInitCond, init_cond_size);	
+	crea_matrice(mat, pInitCond, init_cond_size);
+	new_mat = mat;	
 	
 	delete[] pInitCond;
 	
 	stampa_matrice(mat);
 	
-	
+	while(qualcuno_vivo(new_mat)==true && cont<=max_iter){
+		aggiorna_matrice(mat, new_mat, size);
+		std::cout<<"\n";
+		stampa_matrice(new_mat);
+		new_mat = mat;
+		cont++;
+	}
 	
 	
 	for (int i = 0; i < size; i++) {
         delete[] mat[i];
     }
     delete[] mat;
+    
+    for (int i = 0; i < size; i++) {
+        delete[] new_mat[i];
+    }
+    delete[] new_mat;
 	
 	
 	return 0;
