86af7ceff2d1f35495e80ad4c693411cb89fac34
GabryTremo <gabriele.tremolanti@gmail.com>
2025-03-25 17:27:04 +0100

    crea e stampa matrice

--- Game of life.cpp
+++ Game of life.cpp
@@ -3,22 +3,19 @@
 #include <cmath>
 
 
-int crea_matrice(int *pInitCond, int init_cond_size){
-	
-	int mat[100][100];
+void crea_matrice(int **mat, int *pInitCond, int init_cond_size){
 	
 	for(int i=0; i<100; i++){
 		std::fill(mat[i], mat[i]+100, 0);
 	}
 	
 	for(int j=0; j<init_cond_size; j++){
-		mat[pInitCond[j]][pInitCond[j+1]];
+		mat[pInitCond[j]][pInitCond[j+1]]=1;
 	}
 	
-	return mat[100][100];
 }
 
-void stampa_matrice(int mat[100][100]){
+void stampa_matrice(int **mat){
 	
 	for (int i=0; i<100; i++){
 		for (int j=0; j<100; j++){
@@ -34,9 +31,13 @@ int main() {
 	
 	int *pInitCond = NULL;
 	int init_cond_size;
-	int mat[100][100];
+	int **mat = NULL;
+	int size = 100;
 	
-	
+	mat = new int*[size];
+	for (int i=0; i<size; i++){
+		mat[i] = new int[size];
+	}
 	
 	
 	std::cout<<"Inserisci il numero di cellule vive iniziali: ";
@@ -44,18 +45,23 @@ int main() {
 	pInitCond = new int[2*init_cond_size];
 	std::cout<<"Inserisci le coordinate delle cellule vive iniziali:\n";
 	for (int i=0; i<init_cond_size; i++){
-		//std::cout<<"(";
 		std::cin>>pInitCond[i];
-		//std::cout<<",";
 		std::cin>>pInitCond[i+1];
-		//std::cout<<"); ";
 	}
 	
-	mat[100][100] = crea_matrice(pInitCond, init_cond_size);	
+	crea_matrice(mat, pInitCond, init_cond_size);	
 	
 	delete[] pInitCond;
 	
 	stampa_matrice(mat);
+	
+	
+	
+	
+	for (int i = 0; i < size; i++) {
+        delete[] mat[i];
+    }
+    delete[] mat;
 	
 	
 	return 0;
