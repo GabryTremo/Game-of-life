b79961e9010e9d0ef09e61e8178fbad11017c980
GabryTremo <gabriele.tremolanti@gmail.com>
2025-03-25 16:23:17 +0100

    first commit

---
+++ Game of life.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+int mat[100][100] = crea_matrice(int *pInitCond){
+	
+}
+
+
+int main() {
+	
+	int *pInitCond = NULL;
+	
+	
+	return 0;
+}
